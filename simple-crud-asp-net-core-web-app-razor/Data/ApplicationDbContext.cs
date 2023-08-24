cdbaf38d80fd868b1556aa29495c510634d04a51
Mihai Buga <77620547+mihaibuga@users.noreply.github.com>
2023-08-24 23:13:02 +0200

    Create application db context

---
+++ simple-crud-asp-net-core-web-app-razor/Data/ApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using simple_crud_asp_net_core_web_app_razor.Model;
+
+namespace simple_crud_asp_net_core_web_app_razor.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Category> Category { get; set; }
+    }
+}
