7edff7f8bbec41e026402dd093643f1c77fdeff5
Mihai Buga <77620547+mihaibuga@users.noreply.github.com>
2023-08-24 22:48:30 +0200

    Create Gategory model

---
+++ simple-crud-asp-net-core-web-app-razor/Model/Category.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace simple_crud_asp_net_core_web_app_razor.Model
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public int DisplayOrder { get; set; }
+    }
+}
