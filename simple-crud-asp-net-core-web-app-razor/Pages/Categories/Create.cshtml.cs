01cca198ac1982f5d8ff65e5afa3f0724d0e2ee6
Mihai Buga <77620547+mihaibuga@users.noreply.github.com>
2023-08-25 22:48:43 +0200

    Create new category

--- simple-crud-asp-net-core-web-app-razor/Pages/Categories/Create.cshtml.cs
+++ simple-crud-asp-net-core-web-app-razor/Pages/Categories/Create.cshtml.cs
@@ -1,17 +1,30 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using simple_crud_asp_net_core_web_app_razor.Data;
+using simple_crud_asp_net_core_web_app_razor.Model;
 
 namespace simple_crud_asp_net_core_web_app_razor.Pages.Categories
 {
+    [BindProperties]
     public class CreateModel : PageModel
     {
         private readonly ILogger<CreateModel> _logger;
-        public CreateModel(ILogger<CreateModel> logger)
+        private readonly ApplicationDbContext _db;
+        public Category Category { get; set; }
+        public CreateModel(ILogger<CreateModel> logger, ApplicationDbContext db)
         {
             _logger = logger;
+            _db = db;
         }
         public void OnGet()
         {
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            await _db.Category.AddAsync(Category);
+            await _db.SaveChangesAsync();
+            return RedirectToPage("Index");
         }
     }
 }
